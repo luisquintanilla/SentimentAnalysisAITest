c0649034196eda593320539bead850d7a88f7f82
luisquintanilla <Luis.Quintanilla@microsoft.com>
2025-03-21 11:47:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+var provider = AIProviders.GitHub;
+
+// Construct client
+IChatClient chatClient =
+    provider switch
+    {
+        AIProviders.GitHub => 
+            new ChatCompletionsClient(
+                new Uri("https://models.inference.ai.azure.com"),
+                new AzureKeyCredential(Environment.GetEnvironmentVariable("GH_TOKEN")))
+                .AsChatClient("mistral-small-2503"), // Mistral Small 3.1
+        AIProviders.Ollama => 
+            new OllamaApiClient("http://localhost:11434", "gemma3:4b"),
+        _ => throw new NotImplementedException($"Provider {provider} is not implemented.")
+    };
+
+
+// Get sentiment
+var input = new ReviewSentiment("I love this product!", null);
+
+var response = await chatClient.GetResponseAsync<ReviewSentiment>($"What is the sentiment of this review? {input.Text}");
+
+Console.WriteLine($"Text: {response.Result.Text} | Sentiment: {response.Result.CustomerSentiment}");
+
+record ReviewSentiment(string Text, Sentiment? CustomerSentiment);
+
+public enum Sentiment
+{
+    Positive,
+    Negative,
+    Neutral
+}
+
+public enum AIProviders
+{
+    GitHub,
+    Ollama
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Sentiment Analysis Sample
+
+Sample using GH Models and Ollama with Microsoft.Extensions.AI.
+
+## Changing providers
+
+Update the following code to use the respective provider
+
+```csharp
+var provider = AIProviders.GitHub;
+```
